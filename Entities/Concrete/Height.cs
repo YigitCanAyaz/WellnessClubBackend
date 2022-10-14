bfd0e96dd48c04724b4e7bb759edac52e44754c8
YigitCanAyaz <yigitcanayaz2000@gmail.com>
2022-10-14 13:23:59 +0300

    Added IEntity inheritance for entities/concrete

--- Entities/Concrete/Height.cs
+++ Entities/Concrete/Height.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Entities.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Entities.Concrete
 {
-    public class Height
+    public class Height : IEntity
     {
         public int Id { get; set; }
         public double Meter { get; set; }
