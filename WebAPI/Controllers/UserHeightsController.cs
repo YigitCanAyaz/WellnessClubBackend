019ea379757f4c932f78c11feca0ba2b08d3a3b3
YigitCanAyaz <yigitcanayaz2000@gmail.com>
2022-10-14 19:10:55 +0300

    Added UserHeights and UserWeights controller

---
+++ WebAPI/Controllers/UserHeightsController.cs
@@ -0,0 +1,102 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities.Concrete;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserHeightsController : ControllerBase
+    {
+        private readonly IUserHeightService _userHeightService;
+
+        public UserHeightsController(IUserHeightService userHeightService)
+        {
+            _userHeightService = userHeightService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            var result = _userHeightService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result = _userHeightService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(UserHeight userHeight)
+        {
+            var result = _userHeightService.Add(userHeight);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("update")]
+        public IActionResult Update(UserHeight userHeight)
+        {
+            var result = _userHeightService.Update(userHeight);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("delete")]
+        public IActionResult Delete(UserHeight userHeight)
+        {
+            var result = _userHeightService.Delete(userHeight);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+
+        [HttpGet("getalluserheightdetails")]
+        public IActionResult GetAllUserHeightDetails()
+        {
+            var result = _userHeightService.GetAllUserHeightDetails();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("getuserheightdetailsbyid")]
+        public IActionResult GetUserHeightDetailsById(int id)
+        {
+            var result = _userHeightService.GetUserHeightDetailsById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
---
+++ WebAPI/Controllers/UserWeightsController.cs
@@ -0,0 +1,102 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities.Concrete;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserWeightsController : ControllerBase
+    {
+        private readonly IUserWeightService _userWeightService;
+
+        public UserWeightsController(IUserWeightService userWeightService)
+        {
+            _userWeightService = userWeightService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            var result = _userWeightService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result = _userWeightService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(UserWeight userWeight)
+        {
+            var result = _userWeightService.Add(userWeight);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("update")]
+        public IActionResult Update(UserWeight userWeight)
+        {
+            var result = _userWeightService.Update(userWeight);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("delete")]
+        public IActionResult Delete(UserWeight userWeight)
+        {
+            var result = _userWeightService.Delete(userWeight);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+
+        [HttpGet("getalluserweightdetails")]
+        public IActionResult GetAllUserWeightDetails()
+        {
+            var result = _userWeightService.GetAllUserWeightDetails();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("getuserweightdetailsbyid")]
+        public IActionResult GetUserWeightDetailsById(int id)
+        {
+            var result = _userWeightService.GetUserWeightDetailsById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
