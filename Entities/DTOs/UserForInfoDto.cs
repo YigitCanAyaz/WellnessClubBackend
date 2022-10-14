4cf205062e86e6c8295907fd656d9e5afc3fb042
YigitCanAyaz <yigitcanayaz2000@gmail.com>
2022-10-14 13:35:35 +0300

    Added UserDto's

--- Entities/DTOs/UserForInfoDto.cs
+++ Entities/DTOs/UserForInfoDto.cs
@@ -1,23 +1,19 @@
-﻿using Core.Entities.Abstract;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using Core.Entities;
+using Core.Entities.Abstract;
 
-namespace Core.Entities.Concrete
+namespace Entities.DTOs
 {
-    public class User : IEntity
+    public class UserForInfoDto : IDto
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
-        public byte[] PasswordSalt { get; set; }
-        public byte[] PasswordHash { get; set; }
-        public bool Status { get; set; }
-
         public bool Gender { get; set; }
         public DateTime BirthDate { get; set; }
+        public bool Status { get; set; }
     }
 }
