8aa22d4afcc2ee15749365a9a8fe4da6ef1b7070
YigitCanAyaz <yigitcanayaz2000@gmail.com>
2022-10-14 13:39:48 +0300

    Added UserHeight, UserWeight DTO's

---
+++ Entities/DTOs/UserHeightDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Core.Entities;
+using Core.Entities.Abstract;
+
+namespace Entities.DTOs
+{
+    public class UserHeightDto : IDto
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int HeightId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public bool Gender { get; set; }
+        public DateTime BirthDate { get; set; }
+        public bool Status { get; set; }
+        public float Meter { get; set; }
+    }
+}
