12c1be014e9e3c028a59c67b803411166ab053f3
YigitCanAyaz <yigitcanayaz2000@gmail.com>
2022-10-14 13:12:50 +0300

    Add project files.

---
+++ Core/DependencyResolvers/CoreModule.cs
@@ -0,0 +1,25 @@
+﻿using Core.CrossCuttingConcerns.Caching.Microsoft;
+using Core.CrossCuttingConcerns.Caching;
+using Core.Utilities.IoC;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Http;
+
+namespace Core.DependencyResolvers
+{
+    public class CoreModule : ICoreModule
+    {
+        public void Load(IServiceCollection services)
+        {
+            services.AddMemoryCache();
+            services.AddSingleton<ICacheManager, MemoryCacheManager>();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddSingleton<Stopwatch>();
+        }
+    }
+}
