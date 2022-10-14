4ef036484922476eb164c27c98add894eeac3c7d
YigitCanAyaz <yigitcanayaz2000@gmail.com>
2022-10-14 13:51:25 +0300

    Added abstract data access interfaces

---
+++ DataAccess/Abstract/IUserHeightDal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using Core.DataAccess;
+using Entities.Concrete;
+using Entities.DTOs;
+
+namespace DataAccess.Abstract
+{
+    public interface IUserHeightDal : IEntityRepository<UserHeight>
+    {
+        List<UserHeightDetailDto> GetAllUserHeightDetails(Expression<Func<UserHeightDetailDto, bool>> filter = null);
+        UserHeightDetailDto GetUserHeightDetails(Expression<Func<UserHeightDetailDto, bool>> filter);
+    }
+}
