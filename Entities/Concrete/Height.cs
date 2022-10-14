484315f602fc47cfbe31aba21a7a2f6d527c22df
YigitCanAyaz <yigitcanayaz2000@gmail.com>
2022-10-14 13:22:27 +0300

    Added Entitiy Layer + Concrete Models

---
+++ Entities/Concrete/Height.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Height
+    {
+        public int Id { get; set; }
+        public double Meter { get; set; }
+    }
+}
---
+++ Entities/Concrete/UserWeight.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class UserWeight
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int WeightId { get; set; }
+    }
+}
